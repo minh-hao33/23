03570de9660b9627a7134ad19cb3846e280bb033
Du. Nguyen Trung - CMC Global DKR1 <ntdu@cmcglobal.vn>
2025-03-17 06:20:12 +0000

    API Edit + API Delete Booking

--- HRMS/src/main/java/com/example/hrms/biz/booking/model/Booking.java
+++ HRMS/src/main/java/com/example/hrms/biz/booking/model/Booking.java
@@ -1,15 +1,15 @@
 package com.example.hrms.biz.booking.model;
 
 import com.example.hrms.enumation.BookingStatusEnum;
-import lombok.*;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
 public class Booking {
     private Long bookingId;
     private String username;
